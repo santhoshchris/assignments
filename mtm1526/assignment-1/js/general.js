84c5b68ede945af756775e7f9c5507b14a99923e
unknown <chri0163@WJ310-08.mmserver.ottawa.ad.algonquincollege.com>
2012-05-14 16:40:26 -0400

    finished letter ex

---
+++ mtm1526/assignment-1/js/general.js
@@ -0,0 +1,18 @@
+
+alert('Welcome to the letter game eh');
+
+	var letter;
+
+	while (!letter) {
+		letter = prompt('Enter your favorite letter for some fun! :)');
+	}
+
+	for (var i = 0; i < 10; i++) {
+		for (var j = 0; j <= i; j++) {
+			document.write(letter);
+		}
+
+		document.write('<br>');
+};
+
+letterLooper();
