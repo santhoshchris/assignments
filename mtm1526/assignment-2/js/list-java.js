991a04894be318f85f8a60bc2dac5177712548df
chri0163 <chri0163@algonquinlive.com>
2012-05-24 18:53:12 -0400

    finished to do list

---
+++ mtm1526/assignment-2/js/list-java.js
@@ -0,0 +1,23 @@
+var todos = document.getElementById('todos');
+var item = document.getElementById('item');
+
+document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	if (item.value != '') {
+		var newItem = document.createElement('li');
+
+		newItem.innerHTML = item.value;
+		todos.appendChild(newItem);
+	}
+
+	item.value = '';
+}, false);
+
+todos.addEventListener('click', function (e) {
+	if (e.target.className == 'strikedout') {
+		e.target.className = '';
+	} else {
+		e.target.className = 'strikedout';
+	}
+}, false);
