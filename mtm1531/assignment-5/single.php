18208ef7487ff85fb45148d7eacf3ec7a3fe0852
chri0163 <chri0163@algonquinlive.com>
2012-06-11 16:31:34 -0400

    ff

--- mtm1531/assignment-5/single.php
+++ mtm1531/assignment-5/single.php
@@ -27,10 +27,10 @@ $sql = $db->prepare('SELECT id, title, director, release_date,
 <h1><?php echo $results['title']; ?> </h1>
 
 <dl>
-	<dt>Loves Meat</dt>
+	<dt>Movie Title</dt>
     <dd><?php echo $results['director']; ?></dd>
     <dt>In Jurassic Park</dt>
-    <dd><?php echo $results ['release_date']; ?></dd>
+    <dd><?php echo $results ['release_date'];  var_dump($results); ?></dd>
     </dl>
     
      <a href="delete.php?id=<?php echo $id;?>">Delete</a>
