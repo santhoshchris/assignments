a26d3554fa6ef536c55926e8471d5225f1627fad
chri0163 <chri0163@algonquinlive.com>
2012-06-11 16:38:47 -0400

    changes

--- mtm1531/assignment-5/index.php
+++ mtm1531/assignment-5/index.php
@@ -22,19 +22,15 @@ $results = $sql-> fetchALL();
 <body>
 	<?php foreach ($results as $movies): ?>
     <h2><a href="single.php?id=<?php echo $movies['id']; ?>"> 
-    <h2><?php echo $movies ['title'] ?>  </a> </h2></dd>
+    <h2><?php echo $movies ['title'] ?>  </a> </h2></dd> <p>
+   	 <dt>Director </dt>
+    <dd><?php echo $movies ['director']; ?> <p>
     <dt>Release Date </dt>
     <dd><?php echo $movies ['release_date']; ?> </dd>
 </dl>
 <?php endforeach; ?>
 	
     <h2>
-	<dl>
-    	<dt>Movie Title</dt>
-        <dt></dt>
-        <dt>Director</dt>
-        <dd></dd>
-    </dl>
 
 </body>
 </html>
